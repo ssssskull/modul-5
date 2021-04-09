2dc206809462ffaf053e9994cea4b1f5454d3443
Daniel Papp <danipoker1@gmail.com>
2021-04-09 11:27:09 +0200

    Tilføjede overlay

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,7 @@
 
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector("header");
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
