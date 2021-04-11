3728a8fe8fd2e53aad943bd47c87522891430991
Daniel Papp <danipoker1@gmail.com>
2021-04-11 16:21:25 +0200

    Hero næsten færdig

--- app/js/script.js
+++ app/js/script.js
@@ -2,14 +2,30 @@
 
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector("header");
-const overlay = document.querySelector('.overlay');
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
 
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     // Luk Hamburger Menu
     header.classList.remove("open");
+    body.style.overflow = "auto";
   } else {
     // Åben Hamburger Menu
     header.classList.add("open");
+    body.style.overflow = "hidden";
   }
 });
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    header.style.top = "0";
+  } else {
+    header.style.top = `-${header.clientHeight}px`;
+  }
+  prevScrollpos = currentScrollPos;
+};
