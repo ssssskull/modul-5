89f15c6013387cf5535143f5bc2ebc2d60a511bf
Daniel Papp <danipoker1@gmail.com>
2021-04-16 01:07:01 +0200

    Gået i gang med splash array

--- app/js/splash.js
+++ app/js/splash.js
@@ -1,4 +1,16 @@
-/*const splash = document.querySelector(".splash");
+const splash = document.querySelector(".splash");
+
+const udtalelserArray = [
+  "Det så godt",
+  "Hihi det flot!",
+  "Det var hårdt!",
+  "Tjooooo!",
+  "Hhihiiihi",
+];
+
+const random = Math.floor(Math.random() * udtalelserArray.length);
+console.log(random, udtalelserArray[random]);
+
 // DOMcontentLoaded -- affyrer funktionen når DOMmen er færdig med at loade
 window.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
@@ -6,4 +18,3 @@ window.addEventListener("DOMContentLoaded", function () {
     splash.classList.add("display-none");
   }, 1500);
 });
-*/
