5da6282be7d75808b3529bd7314936755a3535d3
ssssskull <31503598+ssssskull@users.noreply.github.com>
2021-04-16 15:28:36 +0200

    Logo preloader

--- app/js/splash.js
+++ app/js/splash.js
@@ -1,17 +1,6 @@
 const textContainer = document.querySelector(".fade-in");
 const splash = document.querySelector(".splash");
 
-const udtalelserArray = [
-  "Det så godt",
-  "Hihi det flot!",
-  "Det var hårdt!",
-  "Tjooooo!",
-  "Hhihiiihi",
-];
-
-const random = Math.floor(Math.random() * udtalelserArray.length);
-
-textContainer.innerHTML = udtalelserArray[random];
 // DOMcontentLoaded -- affyrer funktionen når DOMmen er færdig med at loade
 window.addEventListener("DOMContentLoaded", function () {
   body.style.overflow = "hidden";
