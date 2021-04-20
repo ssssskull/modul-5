ef79304bd0685e2f0258b65116cc58d686fb84b5
Daniel Papp <danipoker1@gmail.com>
2021-04-20 14:51:19 +0200

    Demonstration af js pensum

--- app/js/script.js
+++ app/js/script.js
@@ -121,6 +121,10 @@ for (i = 0; i < 1; i++) {
 }
 
 // Jakob
+
+// ------- Variabel til videoen
+const omOs = document.querySelector("#om-os");
+
 // ------- Aktiv link navigation
 
 const links = document.querySelectorAll(".links");
@@ -138,3 +142,68 @@ function changeLinkState() {
 
 changeLinkState();
 window.addEventListener("scroll", changeLinkState);
+
+/*----------------------
+------- Pensum --------
+------------------------*/
+
+// ------ Tharshika
+
+// Array
+const pensumArrayTharshika = ["Thomas", "sej", "er han ikke?"];
+pensumArrayTharshika.unshift("Hest!");
+
+// Debugging
+console.log(pensumArrayTharshika);
+
+// Objekt
+const pensumObjektTharshika = {
+  by: "Odense",
+  cykel: "nej!",
+  applewatch: "JAAA",
+};
+console.log(pensumObjektTharshika);
+
+pensumObjektTharshika.tired = "yes";
+
+console.log(pensumObjektTharshika);
+
+// If else
+const x = 2;
+
+if (x === 2) {
+  console.log("x er lige med 2");
+} else {
+  console.log("x ikke er lige med 2");
+}
+
+// For loop
+for (let i = 0; i < pensumArrayTharshika.length; i++) {
+  console.log("Tharshikas forloop!");
+}
+
+// ------ Daniel
+
+// Array
+const pensumArrayDaniel = ["🚗", "🚓", "🚕"];
+pensumArrayDaniel.splice(1, 1);
+console.log(pensumArrayDaniel);
+// Array er også et objekt, men jeg vælger at lave et alligevel
+
+// Objekt
+const pensumObjektDaniel = {
+  speed: "fast",
+  food: "none",
+};
+
+// Debugging
+console.log(pensumObjektDaniel);
+pensumObjektDaniel.tired = "very :(";
+console.log(pensumObjektDaniel);
+
+console.log(pensumArrayDaniel.length);
+// For loop
+
+for (let y = 0; y < pensumArrayDaniel.length; y++) {
+  console.log("Daniels forloop!");
+}
